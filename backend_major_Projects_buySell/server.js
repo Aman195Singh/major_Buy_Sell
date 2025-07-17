010aaf5a5aa14ed59c98a158e06796dee3a703e8
Aman Singh <jgdv.amansingh3107@gmail.com>
2025-07-17 10:02:43 +0530

    updated server.js

--- backend_major_Projects_buySell/server.js
+++ backend_major_Projects_buySell/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/auth",authRoutes);
-// app.use("/api/products",productRoutes);
+app.use("/api/products",productRoutes);
 
 connectDB().then(()=> {
     app.listen(port, ()=>{
